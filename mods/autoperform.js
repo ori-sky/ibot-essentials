c6cd1a88f7286a998aa0c6cd83a6dd7cf758652b
Shockk <shokku.ra@gmail.com>
2013-12-22 19:01:55 +0000

    mods/autoperform: Wrote module to perform IRC messages on connect.

---
+++ mods/autoperform.js
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2013 David Farrell
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+var config = undefined
+
+exports.name = 'ibot_autoperform'
+exports.config$load = function(cfg)
+{
+    config = cfg
+}
+
+exports.ibot_core$001 = function(server, message)
+{
+    if(Array.isArray(config))
+    {
+        for(var i=0; i<config.length; ++i)
+        {
+            server.send(config[i])
+        }
+    }
+}
