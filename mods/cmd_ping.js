691a055a1c0d19664b57afcca4f3b7228d3a0ffe
Shockk <shokku.ra@gmail.com>
2013-12-22 18:20:59 +0000

    mods/ping: Replaced core.privmsg call with privmsg.reply.

--- mods/cmd_ping.js
+++ mods/cmd_ping.js
@@ -18,5 +18,5 @@ exports.name = 'ibot_cmd_ping'
 
 exports.ibot_cmd$cmd__ping = function(server, privmsg, command)
 {
-    exports.__.ibot_core.privmsg(server, privmsg.target, 'pong')
+    privmsg.reply(server, 'pong')
 }
