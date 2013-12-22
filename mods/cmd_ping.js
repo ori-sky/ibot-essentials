e93f7d182bf6faea3b41036f365893fc05d280c8
Shockk <shokku.ra@gmail.com>
2013-12-22 02:14:45 +0000

    mods/cmd_ping: Switched to use shorthand reference to modules.

--- mods/cmd_ping.js
+++ mods/cmd_ping.js
@@ -2,5 +2,5 @@ exports.name = 'ibot_cmd_ping'
 
 exports.ibot_cmd$cmd__ping = function(server, privmsg, command)
 {
-    exports.__mods.modules.ibot_core.privmsg(server, privmsg.target, 'pong')
+    exports.__.ibot_core.privmsg(server, privmsg.target, 'pong')
 }
