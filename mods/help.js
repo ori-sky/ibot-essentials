5b5c14aef7cce8e1f7a3639d0236db39efabc137
Shockk <shokku.ra@gmail.com>
2014-06-12 23:21:41 +0000

    Call methods through mods instead of manually

--- mods/help.js
+++ mods/help.js
@@ -18,7 +18,7 @@ exports.name = 'ibot_help'
 
 exports.help = function(topic)
 {
-	var obj = exports.__mods.fire(exports.name + '$help_' + topic)
+	var obj = exports.__mods.fire('help_' + topic)
 	var keys = Object.keys(obj)
 
 	for(var i=0; i<keys.length; ++i)
